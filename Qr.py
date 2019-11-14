55ee7c11aefe9df9a8befd6c0b5b1c673cc0bff5
rauchdimehdi <medy-rauchdi@hotmail.fr>
2019-11-14 12:37:52 +0100

    add an explaination

--- Qr.py
+++ Qr.py
@@ -1,8 +1,10 @@
 import pyqrcode
 
 def qrcode():
+	#here you show what you want the scanner to get
 	q=pyqrcode.create('My name is Mehdi Rauchdi')
 	q.png('my.png',scale=6)
+	#just to see this message when the program will be executed
 	print('QR code is generated')
 
 if __name__ == '__main__':
